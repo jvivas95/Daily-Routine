4d6d494d759fcfbfd45fb9c9b7b8acc556b6aac8
jeff <jefferson.vivas.95@gmail.com>
2025-03-10 00:26:02 +0100

    Reestructuración de la pagina de inicio, separación de los componentes

--- vistas/inc/header.php
+++ vistas/inc/header.php
@@ -1,27 +1,37 @@
 <?php 
-  
-  include_once "./config/config.php";
-  
+  include_once "./config/config.php"; 
 ?>
 <!doctype html>
 <html lang="es" data-bs-theme="auto">
 <head>
+  <!-- Scripts -->
+  <script src="<?php echo(BASE_URL) ?>/assets/js/color-modes.js"></script>
+
+  <!-- Meta Tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="description" content="">
   <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
   <meta name="generator" content="Hugo 0.122.0">
-  <title>DAYLY ROUTINE</title>
+  <title>DAILY ROUTINE</title>
 
+  <!-- Canonical Link -->
+  <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/carousel/">
+  
+  <!-- Stylesheets -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
   <link href="<?php echo(BASE_URL) ?>/assets/css/bootstrap.min.css" rel="stylesheet">
   <link href="<?php echo(BASE_URL) ?>/assets/css/carousel.css" rel="stylesheet">
 </head>
 <body>
-<!-- CABECERA -->    
+  <!-- SVG Symbols -->
+  <svg xmlns="http://www.w3.org/2000/svg" class="d-none">
+  </svg>
+<!-- Header -->
 <header data-bs-theme="dark">
   <nav class="navbar navbar-expand-md navbar-dark bg-dark">
     <div class="container-fluid">
-      <img src="<?php echo (BASE_URL) ?>/assets/img/logo.png" style="width: 10%; padding: 5px;"/>
+      <img src="<?php echo (BASE_URL) ?>/assets/img/logo.png" style="width: 10%; padding: 5px;" alt="Logo"/>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -29,7 +39,7 @@
       <div class="collapse navbar-collapse" id="navbarCollapse">
         <ul class="navbar-nav me-auto mb-2 mb-md-0">
           <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="/rutinasApp-proyecto/index.php">HOME</a>
+            <a class="nav-link active" aria-current="page" href="/rutinasApp-proyecto/index.php">HOME</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="/rutinasApp-proyecto/vistas/contact.php">CONTACTO</a>
