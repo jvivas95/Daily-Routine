56d5b60dc7c0d8807e2815d6e392394b44fd0b7b
Jeff <jefferson.vivas.95@gmail.com>
2025-03-30 16:04:16 +0200

    Mod. rutas 1.1

--- vistas/inc/header.php
+++ vistas/inc/header.php
@@ -3,7 +3,7 @@
   include_once __DIR__."/../../config/config.php";
 
   if (!isset($_SESSION["usuario"]) && basename($_SERVER['SCRIPT_NAME']) != 'index.php') {
-    header("Location: ".BASE_URL."/vistas/login.php");
+    header("Location: /vistas/login.php");
     exit();
 }
 
@@ -12,7 +12,7 @@
 <html lang="es" data-bs-theme="auto">
 <head>
   <!-- Scripts -->
-  <script src="<?php echo(BASE_URL) ?>/assets/js/color-modes.js"></script>
+  <script src="/assets/js/color-modes.js"></script>
 
   <!-- Meta Tags -->
   <meta charset="utf-8">
@@ -38,8 +38,8 @@
 <header data-bs-theme="dark">
   <nav class="navbar navbar-expand-md navbar-dark bg-dark">
     <div class="container-fluid">
-      <a href="<?php echo BASE_URL?>/index.php">
-        <img src="<?php echo (BASE_URL) ?>/assets/img/logo.png" style="width: 25%; padding: 5px;" alt="Logo"/>
+      <a href="/index.php">
+        <img src="/assets/img/logo.png" style="width: 25%; padding: 5px;" alt="Logo"/>
       </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -48,7 +48,7 @@
         <ul class="navbar-nav ms-auto mb-2 mb-md-0">
           <?php if (Autenticacion::estaAutenticado()){?>
             <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="<?php echo(BASE_URL)?>/vistas/verRutinas.php">
+            <a class="nav-link active" aria-current="page" href="/vistas/verRutinas.php">
               <?php echo Autenticacion::obtenerUsuario(); ?>
             </a>
           </li>
@@ -56,14 +56,14 @@
               <div style="border-top: 1px solid white; width: 100%;"></div>
             </li>
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="<?php echo(BASE_URL)?>/vistas/logout.php"> Cerrar sesión</a>
+            <a class="nav-link active" aria-current="page" href="/vistas/logout.php"> Cerrar sesión</a>
           </li>
           <?php } else { ?>
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="<?php echo(BASE_URL)?>/vistas/login.php">Iniciar Sesión / </a>
+            <a class="nav-link active" aria-current="page" href="/vistas/login.php">Iniciar Sesión / </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="<?php echo(BASE_URL)?>/vistas/registro.php">Registrarse</a>
+            <a class="nav-link active" aria-current="page" href="/vistas/registro.php">Registrarse</a>
           </li>
           <?php } ?>
           <!-- AÑADIR MAS ITEMS
